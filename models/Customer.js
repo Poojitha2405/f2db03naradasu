e0550d514ad8855391488d73d5775b3624c90d4b
Poojitha2405 <npoojitha2405@gmail.com>
2023-04-27 22:41:00 -0500

    added required changes

--- models/Customer.js
+++ models/Customer.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const CustomerSchema = mongoose.Schema({
     Cust_Name: String,
-    Cust_Age: Number,
-    Mail_Id: String
+    Cust_Age: {type: Number, min: 1, max: 100},
+    Mail_Id: {type:String, required:true}
 })
 module.exports = mongoose.model("Customer",
 CustomerSchema)
--- routes/Customer.js
+++ routes/Customer.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const Customer_controlers= require('../controllers/Customer');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET Customers */
 router.get('/', Customer_controlers.Customer_view_all_Page );
 
@@ -9,11 +18,11 @@ router.get('/customers/:id', Customer_controlers.Customer_detail);
 /* GET detail customer page */
 router.get('/detail', Customer_controlers.Customer_view_one_Page);
 
-router.get('/create', Customer_controlers.Customer_create_Page);
+router.get('/create', secured, Customer_controlers.Customer_create_Page);
 
-router.get('/update', Customer_controlers.Customer_update_Page);
+router.get('/update', secured, Customer_controlers.Customer_update_Page);
 
-router.get('/delete', Customer_controlers.Customer_delete_Page);
+router.get('/delete', secured, Customer_controlers.Customer_delete_Page);
 
 module.exports = router;
 
